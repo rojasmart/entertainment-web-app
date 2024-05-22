4812a874cad31c810e4f7a56d2ee8bb35f273573
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-05-22 21:38:12 +0100

    --add firebase logout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,20 @@
 import { Button, Text, Box } from "@chakra-ui/react";
+import { useContext } from "react"; // Import useContext hook from React
+import { AuthGoogleContext } from "../../contexts/authGoogle"; // Import your AuthGoogleContext
 
 export const Home = () => {
+  const { signOut } = useContext(AuthGoogleContext); // Access signOut function from the context
+
+  const handleLogout = () => {
+    signOut(); // Call signOut function
+  };
+
   return (
     <Box>
       <Text fontSize={"6xl"} color={"white"}>
         Home
       </Text>
-      <Button>Log out</Button>
+      <Button onClick={handleLogout}>Log out</Button>
     </Box>
   );
 };
