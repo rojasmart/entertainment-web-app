6c91d6a109c456b08b6f3d143d06a2c3afcc7737
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-05-22 20:40:48 +0100

    --add firebase config file auth

---
+++ src/routes/routes.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
+
+export const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
--- src/services/FireBaseConfig.js
+++ src/services/FireBaseConfig.js
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyBGF08cHmbjK26egFhMbOOifacmk4yE78w",
+  authDomain: "entertainment-app-93f48.firebaseapp.com",
+  projectId: "entertainment-app-93f48",
+  storageBucket: "entertainment-app-93f48.appspot.com",
+  messagingSenderId: "470358200380",
+  appId: "1:470358200380:web:47014fc9bc40d3d431b339",
 };
 
 export const app = initializeApp(firebaseConfig);
