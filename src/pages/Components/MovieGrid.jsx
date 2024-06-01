25c93c1ae67c9a5c4d9d62c42c7feb63a41a4ed9
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-06-01 08:36:37 +0100

    --added new slider for trending area

--- src/pages/Components/MovieGrid.jsx
+++ src/pages/Components/MovieGrid.jsx
@@ -1,60 +1,80 @@
 import { Grid, Box } from "@chakra-ui/react";
 
-import { SearchIcon } from "@chakra-ui/icons";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export const MovieGrid = () => {
   return (
-    <Grid templateColumns="repeat(4, 1fr)" gap={6}>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
+    <>
+      <Box mb={5}>
+        <Carousel showThumbs={false}>
+          <Box bg="white" height="200px" borderRadius="md" p={3}>
+            {/* Card content goes here */}
+          </Box>
+          <Box bg="white" height="200px" borderRadius="md" p={3}>
+            {/* Card content goes here */}
+          </Box>
+          <Box bg="white" height="200px" borderRadius="md" p={3}>
+            {/* Card content goes here */}
+          </Box>
+          <Box bg="white" height="200px" borderRadius="md" p={3}>
+            {/* Card content goes here */}
+          </Box>
+          {/* Add more cards as needed */}
+        </Carousel>
       </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
-      <Box bg="white" height="200px" borderRadius="md" p={3}>
-        {/* Card content goes here */}
-      </Box>
+      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
+        <Box bg="white" height="200px" borderRadius="md" p={3}>
+          {/* Card content goes here */}
+        </Box>
 
-      {/* Add more cards as needed */}
-    </Grid>
+        {/* Add more cards as needed */}
+      </Grid>
+    </>
   );
 };
