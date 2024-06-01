962717c8cba563a7824e08f6378dbde4655443bb
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-06-01 09:06:44 +0100

    --fix horizontal scroll for trending area

--- src/pages/Components/MovieGrid.jsx
+++ src/pages/Components/MovieGrid.jsx
@@ -1,28 +1,78 @@
 import { Grid, Box } from "@chakra-ui/react";
+import DragScroll from "react-drag-scroll";
 
-import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export const MovieGrid = () => {
   return (
     <>
-      <Box mb={5}>
-        <Carousel showThumbs={false}>
-          <Box bg="white" height="200px" borderRadius="md" p={3}>
-            {/* Card content goes here */}
-          </Box>
-          <Box bg="white" height="200px" borderRadius="md" p={3}>
-            {/* Card content goes here */}
-          </Box>
-          <Box bg="white" height="200px" borderRadius="md" p={3}>
-            {/* Card content goes here */}
-          </Box>
-          <Box bg="white" height="200px" borderRadius="md" p={3}>
-            {/* Card content goes here */}
-          </Box>
-          {/* Add more cards as needed */}
-        </Carousel>
-      </Box>
+      <DragScroll>
+        <Box overflowX="scroll" whiteSpace="nowrap">
+          <Grid templateColumns="repeat(6, 1fr)" gap={6} py={6}>
+            <Box
+              bg="white"
+              height="200px"
+              borderRadius="md"
+              p={3}
+              minW="500px"
+              display="inline-block"
+            >
+              {/* Card content goes here */}
+            </Box>
+            <Box
+              bg="white"
+              height="200px"
+              borderRadius="md"
+              p={3}
+              minW="500px"
+              display="inline-block"
+            >
+              {/* Card content goes here */}
+            </Box>
+            <Box
+              bg="white"
+              height="200px"
+              borderRadius="md"
+              p={3}
+              minW="500px"
+              display="inline-block"
+            >
+              {/* Card content goes here */}
+            </Box>
+            <Box
+              bg="white"
+              height="200px"
+              borderRadius="md"
+              p={3}
+              minW="500px"
+              display="inline-block"
+            >
+              {/* Card content goes here */}
+            </Box>
+            <Box
+              bg="white"
+              height="200px"
+              borderRadius="md"
+              p={3}
+              minW="500px"
+              display="inline-block"
+            >
+              {/* Card content goes here */}
+            </Box>
+            <Box
+              bg="white"
+              height="200px"
+              borderRadius="md"
+              p={3}
+              minW="500px"
+              display="inline-block"
+            >
+              {/* Card content goes here */}
+            </Box>
+            {/* Add more cards as needed */}
+          </Grid>
+        </Box>
+      </DragScroll>
       <Grid templateColumns="repeat(4, 1fr)" gap={6}>
         <Box bg="white" height="200px" borderRadius="md" p={3}>
           {/* Card content goes here */}
