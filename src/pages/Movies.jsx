c50d0dc7d1ecbff74d31ba0a91eed860611fdba7
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-06-17 21:04:51 +0100

    --add search input on movies working

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useEffect, useState } from "react";
 import { Container } from "@chakra-ui/react";
 import { Layout } from "../pages/Home";
@@ -8,16 +9,31 @@ import { getMovies } from "../api/Auth";
 export const Movies = () => {
   const [movies, setMovies] = useState([]);
 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = useCallback((term) => {
+    setSearchTerm(term);
+  }, []);
+
   useEffect(() => {
     getMovies().then((data) => setMovies(data.results));
-    //getMoviesPopular().then((data) => setMovies(data.results));
   }, []);
+
+  const filteredMovies = useCallback(() => {
+    return movies.filter((movie) =>
+      movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [movies, searchTerm]);
 
   return (
     <Layout isMoviePage={false}>
       <Container maxW={"100%"}>
-        <SearchInput />
-        <MovieGrid text="Movies" useScrollContainer={false} movies={movies} />
+        <SearchInput onSearch={handleSearch} />
+        <MovieGrid
+          text="Movies"
+          useScrollContainer={false}
+          movies={filteredMovies()}
+        />
       </Container>
     </Layout>
   );
